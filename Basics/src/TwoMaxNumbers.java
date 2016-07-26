cb1435145b219c0a547e1300a60fe56fc9d6c51b
Samyak Bhuta <samyak.bhuta@gmail.com>
2016-07-26 09:22:29 +0530

    First program to find two highest numbers in array

---
+++ Basics/src/TwoMaxNumbers.java
@@ -0,0 +1,39 @@
+/**
+ * Created by shubham on 7/26/16.
+ */
+
+/**
+ * Java program to find two maximum numbers in a array
+ */
+public class TwoMaxNumbers {
+
+    public static void main(String args[]) {
+
+        System.out.println("Executing function to find two highest numbers in a array");
+        int[] numbers = {1,2,3,5,89,56};
+        FindTwoHighestNumbers(numbers);
+
+    }
+
+    /**
+     * function to find highest numbers by traversing the array
+     */
+    private static void FindTwoHighestNumbers(int[] nums) {
+
+        int maxOne = 0;
+        int maxTwo = 0;
+
+        for (int n : nums) {
+            if (maxOne < n) {
+                maxTwo = maxOne;
+                maxOne = n;
+            } else if (maxTwo < n) {
+                maxTwo = n;
+            }
+        }
+
+        System.out.println("The highest number is : " + maxOne);
+        System.out.println("The second highest number is : " + maxTwo);
+
+    }
+}
